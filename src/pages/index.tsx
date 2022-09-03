3935c0e975607cb489ddb89ca8e003f4e6d99f57
Giovanna Linda <eugiovannasouza@gmail.com>
2022-09-03 12:26:09 -0300

    feat: get days and seconds

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+import {
+  differenceInDays,
+  differenceInMonths,
+  differenceInSeconds,
+  getSeconds,
+  subSeconds,
+} from 'date-fns'
+import { useEffect, useState } from 'react'
+
 import styled from 'styled-components'
 
 export const Main = styled.main`
@@ -14,6 +23,24 @@ export const Main = styled.main`
 `
 
 const Home: NextPage = () => {
+  const [currentDateTime, setCurrentDateTime] = useState(new Date())
+  const [firstDay, setFirstDay] = useState(new Date(2020, 8, 2))
+  const [seconds, setSeconds] = useState(0)
+  const [days, setDays] = useState(0)
+
+  useEffect(() => {
+    const id = setInterval(() => setCurrentDateTime(new Date()), 1000)
+    return () => {
+      clearInterval(id)
+    }
+  }, [])
+
+  useEffect(() => {
+    setSeconds(differenceInSeconds(currentDateTime, firstDay))
+    setDays(differenceInDays(currentDateTime, firstDay))
+    console.log({ seconds, days })
+  }, [currentDateTime, firstDay, days, seconds])
+
   return (
     <div>
       <Head>
@@ -22,11 +49,10 @@ const Home: NextPage = () => {
       </Head>
 
       <Main>
-        <h1>Hello World 🥳</h1>
-
-        <p>
-          Get started by editing <code>pages/index.tsx</code>
-        </p>
+        <>
+          <h1>{`${currentDateTime.toLocaleDateString()} ${currentDateTime.toLocaleTimeString()}`}</h1>
+          <h1>{`${currentDateTime.toLocaleDateString()} ${currentDateTime.toLocaleTimeString()}`}</h1>
+        </>
       </Main>
     </div>
   )
